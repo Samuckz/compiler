ab3eefbf5a7441685b2bb7727d373e31376965b0
Samuel Santana <contato.santanasamuel@gmail.com>
2026-04-23 17:58:49 -0300

    pequeno ajuste na classe Word

--- src/models/Word.java
+++ src/models/Word.java
@@ -5,7 +5,7 @@ public class Word extends Token{
 
     public static final Word  and = new Word ("&&", Tag.AND);
     public static final Word  or = new Word ("||", Tag.OR);
-    public static final Word  eq = new Word ("==", Tag.EQ);
+    public static final Word  eq = new Word (":=", Tag.EQ);
     public static final Word  ne = new Word ("!=", Tag.NE);
     public static final Word  le = new Word ("<=", Tag.LE);
     public static final Word  ge = new Word (">=", Tag.GE);
